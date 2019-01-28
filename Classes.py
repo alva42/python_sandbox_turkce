37761a9aa17454f8b81ca3b4a5e1e169ce70d235
E.K. Kot <40169052+alva42@users.noreply.github.com>
2019-01-28 15:11:35 +0300

    Delete Classes.py

--- Classes.py
+++
@@ -1,2 +0,0 @@
-# Sınıflar(classes), nesneler(objects) için bir taslak gibidir. Bir nesne(object) onunla ilişkili özelliklere ve fonksiyonlara(metodlara) sahiptir. Python'da neredeyse her şey bir nesne(object)dir.
-

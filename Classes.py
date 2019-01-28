f51b096a654ef0c863616dce02d53215ec1a5080
E.K. Kot <40169052+alva42@users.noreply.github.com>
2019-01-28 15:09:43 +0300

    Create Classes.py

---
+++ Classes.py
@@ -0,0 +1,2 @@
+# Sınıflar(classes), nesneler(objects) için bir taslak gibidir. Bir nesne(object) onunla ilişkili özelliklere ve fonksiyonlara(metodlara) sahiptir. Python'da neredeyse her şey bir nesne(object)dir.
+
